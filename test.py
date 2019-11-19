d4e3d58c126a053f5ede81b26c69cf88f953a7b6
Solomich <368772@mcpsmd.org>
2019-11-19 08:47:55 -0500

    created for loop that tells if it is high or low

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
 import random
 randomNumber= random.randint(0,101)
 correctGuess= False
-while correctGuess=False:
+while correctGuess==False:
     str_guess= input("Guess a number 1-100")
     guess=int(str_guess)
+    if guess==randomNumber:
+        correctGuess=True
+        print("Great guess! Correct guess")
+    elif guess>randomNumber:
+        print("Guess is too high")
+    elif guess<randomNumber:
+        print("Guess is too low")
