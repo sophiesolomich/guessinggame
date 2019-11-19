36537b01377c7022a96788dc6911a89e04caa3db
Solomich <368772@mcpsmd.org>
2019-11-19 08:50:50 -0500

    added spacing

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import random
 randomNumber= random.randint(0,101)
 correctGuess= False
 while correctGuess==False:
-    str_guess= input("Guess a number 1-100")
+    str_guess= input("Guess a number 1-100: ")
     guess=int(str_guess)
     if guess==randomNumber:
         correctGuess=True
